05c352599c1c938a4b36c1d82efd02413bb662da
jonaisenberg <jonathan.araos.a@gmail.com>
2024-03-01 20:24:31 -0300

    my first commit

---
+++ Backend/server/index.js
@@ -0,0 +1,63 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const cors = require("cors")
+
+const { registrarUsuario, verificarCredenciales, obtenerUsuario } = require("../utils/consultas")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/usuarios', async (req, res) => {
+    try {
+        const usuario = req.body
+        await registrarUsuario(usuario)
+        console.log("Usuario registrado:", usuario.email)
+        res.sendStatus(201)
+    } catch (error) {
+        console.error("Error al registrar usuario:", error)
+        res.status(error.code || 500).send(error)
+    }
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        await verificarCredenciales(email, password)
+        const token = jwt.sign({ email }, "az_AZ")
+        console.log("Inicio de sesión exitoso para:", email)
+        res.json({token})
+        console.log('aqui el token',token)
+    } catch (error) {
+        console.error("Error al iniciar sesión:", error)
+        res.status(error.code || 500).send(error)
+    }
+})
+
+app.get("/usuarios", async (req, res) => {
+    const token = req.headers.authorization
+console.log('holaToken',token)
+    if (!token) {
+        return res.status(401).json({ error: "Token de autorización no proporcionado" })
+    }
+
+    try {
+        const decoded = jwt.verify(token.split(" ")[1], "az_AZ")
+        const usuario = await obtenerUsuario(decoded.email)
+
+        if (usuario.error) {
+            res.status(404).json({ error: "Usuario no encontrado" })
+        } else {
+            res.json(usuario)
+        }
+    } catch (error) {
+        console.error("Error al obtener usuario:", error)
+        res.status(500).json({ error: "Error interno del servidor" })
+    }
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
---
+++ Backend/utils/consultas.js
@@ -0,0 +1,68 @@
+const dotenv = require('dotenv')
+const bcrypt = require('bcryptjs')
+const { Pool } = require('pg')
+
+dotenv.config()
+
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    allowExitOnIdle: true
+})
+
+const obtenerRegistroId = async () => {
+    const consulta = "SELECT * FROM usuarios;"
+    const { rows } = await pool.query(consulta)
+    return rows
+};
+
+const registrarUsuario = async (usuario) => {
+    try {
+        let { email, password, rol, lenguage } = usuario
+        const passwordEncriptada = bcrypt.hashSync(password)
+        password = passwordEncriptada;
+        const values = [email, passwordEncriptada, rol, lenguage]
+        const consulta = "INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4) RETURNING *;"
+        await pool.query(consulta, values)
+        console.log("Usuario registrado:", email)
+    } catch (error) {
+        console.log(error)
+        return { code: 500, error }
+    }
+}
+
+const verificarCredenciales = async (email, password) => {
+    const values = [email];
+    const consulta = "SELECT * FROM usuarios WHERE email = $1"
+    const { rows: [usuario], rowCount } = await pool.query(consulta, values)
+
+    if (rowCount === 0) {
+        console.log('No se encontró registro con estos datos')
+        throw { code: 401, message: "Email o contraseña incorrecta" }
+    }
+
+    const { password: passwordEncriptada } = usuario
+    const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada)
+
+    if (!passwordEsCorrecta) {
+        throw { code: 401, message: "Email o contraseña incorrecta" }
+    }
+}
+
+const obtenerUsuario = async (email) => {
+    try {
+        const consulta = "SELECT * FROM usuarios WHERE email = $1"
+        const { rows } = await pool.query(consulta, [email])
+        const usuario = rows[0]
+        console.log('Usuario encontrado:', usuario)
+        return usuario
+    } catch (error) {
+        console.error("Error al obtener usuario por Email:", error)
+        throw error
+    }
+}
+
+module.exports = { registrarUsuario, obtenerRegistroId, verificarCredenciales, obtenerUsuario };
